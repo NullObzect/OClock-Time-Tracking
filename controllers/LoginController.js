d2e38084acfc7120b0e987303c17943ae388ae72
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2021-09-06 20:08:31 +0600

    express validator for login page
    create 1 file name "loginValidator" in "validator" folder

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -1,7 +1,25 @@
+const { validationResult } = require('express-validator');
+
 const LoginController = {
   getLogin: (req, res) => {
     res.render('pages/login')
   },
+  loginController: async (req, res) => {
+    console.log('====> login data', req.body);
+    const { userMail, userPass } = req.body;
+
+    const errors = validationResult(req).formatWith((error) => error.msg)
+    if (!errors.isEmpty()) {
+      return res.render('pages/login', { error: errors.mapped(), value: { userMail, userPass } })
+    }
+    try {
+
+    } catch (err) {
+      console.log('====> Error form loginController', err);
+      return err;
+    }
+  },
+
 }
 
 module.exports = LoginController
--- routers/loginRoute.js
+++ routers/loginRoute.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const LoginController = require('../controllers/LoginController')
 const decorateHtmlResponse = require('../middleware/decorateHtmlResponse')
+const { loginValidator } = require('../middleware/validator/loginValidator')
 
 router.get('/', decorateHtmlResponse('Login'), LoginController.getLogin)
+router.post('/', decorateHtmlResponse('Login'), loginValidator, LoginController.loginController)
 module.exports = router
