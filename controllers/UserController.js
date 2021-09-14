777b5fd50b49c93cb2d8d6e40353285b3372b35c
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2021-09-14 22:52:05 +0600

    form validation , UI table , show user ,delete user
    user form validation client side,
    create UI Table,
    show all users, admin and  users in table
    admin  delete users

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -47,6 +47,42 @@ const UserController = {
       return err;
     }
   },
+  // show all users list
+  allUsersList: async (req, res) => {
+    const users = await UserModel.getAllUsersList()
+
+    res.render('pages/allUsers', { users })
+  },
+  // show user list
+  usersList: async (req, res) => {
+    const users = await UserModel.getUsersList()
+    res.render('pages/usersList', { users })
+  },
+  // show admin list
+  adminList: async (req, res) => {
+    try {
+      const admins = await UserModel.getAdminList()
+      res.render('pages/adminList', { admins })
+    } catch (err) {
+      console.log('====>Error form Admin list controller', err);
+    }
+  },
+  // Delete a user
+  deleteUser: async (req, res) => {
+    try {
+      const userId = req.params.id
+      const isUserDelete = await UserModel.getDeleteUser(userId)
+      if (isUserDelete.errno) {
+        res.send('Error')
+      } else {
+        res.render('pages/allUsers')
+      }
+    } catch (err) {
+      console.log('====>Error form Delet controller', err);
+      return err;
+    }
+  },
+
 };
 
 module.exports = UserController;
--- src/index.js
+++ src/index.js
@@ -19,3 +19,4 @@ for (let i = 0; i < menuItem.length; i++) {
     list[i].className = 'list active'
   }
 }
+
