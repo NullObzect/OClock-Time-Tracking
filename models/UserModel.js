0fbdea0c88f3097f09431bcb3cb7a7dff256df49
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2021-09-17 19:30:13 +0600

    add avatar in userModel

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,11 +1,11 @@
 const dbConnect = require('../config/database');
 
 const UserModel = {
-  registerModel: async (userName, userPhone, userRole, userMail, userPass) => {
+  registerModel: async (userName, userPhone, userRole, userMail, userPass, avatar) => {
     try {
-      const registerQuery = 'INSERT INTO `users`( `user_name`, `user_phone`, `user_role`, `user_mail`, `user_pass`) VALUES (?,?,?,?,?)';
+      const registerQuery = 'INSERT INTO `users`( `user_name`, `user_phone`, `user_role`, `user_mail`, `user_pass`,`avatar`) VALUES (?,?,?,?,?,?)';
 
-      const values = [userName, userPhone, userRole, userMail, userPass];
+      const values = [userName, userPhone, userRole, userMail, userPass, avatar];
 
       const [rows] = await dbConnect.promise().execute(registerQuery, values);
       return rows;
