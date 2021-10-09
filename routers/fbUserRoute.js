ebac10ececfd5550f66f6b0a51c3091d5acdf448
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2021-10-10 00:02:35 +0600

    created fbUserRouter.js

---
+++ routers/fbUserRoute.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const passport = require('passport')
+const FbUserController = require('../controllers/FbUserController');
+
+require('../config/passportFB')(passport)
+
+router.get('/profile', FbUserController.profile)
+// router.get('/profile', (req, res) => { res.send('This is profile page ') })
+router.get('/error', FbUserController.error)
+
+// passport router
+router.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email,user_photos' }));
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/error' }), FbUserController.profile);
+
+router.get('/remove/facebook', FbUserController.facebookUserDelete)
+
+module.exports = router;
