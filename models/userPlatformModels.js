ca3cf8a8f261dabd0eed5a4d6044ae0d4b62b3a5
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2021-10-12 11:16:32 +0600

    update UserPlatformModel

--- models/userPlatformModels.js
+++ models/userPlatformModels.js
@@ -2,13 +2,13 @@ const dbConnect = require('../config/database')
 
 const userPlatformModel = {
   googlePlatformSet: async (id, platform, uniqueId, name, avatar) => {
-    const userConnectionQuery = 'INSERT INTO users_connection_details ( user_id, user_platform, user_unique_id,user_username,user_avatar) VALUES (?,?,?,?,?)'
+    const userConnectionQuery = 'INSERT INTO users_connection_details ( user_id, platform, unique_info,user_username,user_avatar) VALUES (?,?,?,?,?)'
     const value = [id, platform, uniqueId, name, avatar]
     const [rows] = await dbConnect.promise().execute(userConnectionQuery, value)
     return rows
   },
   googlePlatformRemove: async (id) => {
-    const userConDeleteQuery = 'DELETE FROM `users_connection_details` WHERE user_platform = "google" AND user_id = ?'
+    const userConDeleteQuery = 'DELETE FROM `users_connection_details` WHERE platform = "google" AND user_id = ?'
     const value = [id]
     const [rows] = await dbConnect.promise().execute(userConDeleteQuery, value)
     return rows
@@ -20,13 +20,13 @@ const userPlatformModel = {
     return rows
   },
   getGoogleUserByMail: async (mail) => {
-    const sql = 'SELECT user_id FROM `users_connection_details` WHERE user_unique_id = ?'
+    const sql = 'SELECT user_id FROM `users_connection_details` WHERE unique_info = ?'
     const value = [mail]
     const [rows] = await dbConnect.promise().execute(sql, value)
     return rows
   },
   googleUserUpdate: async (userName, pic, id) => {
-    const updateUserSql = 'UPDATE `users_connection_details` SET user_username=?,user_avatar = ? WHERE   user_unique_id =? AND user_platform = "google"'
+    const updateUserSql = 'UPDATE `users_connection_details` SET user_username=?,user_avatar = ? WHERE   unique_info =? AND platform = "google"'
     const value = [userName, pic, id]
     const [rows] = await dbConnect.promise().execute(updateUserSql, value)
     return rows
