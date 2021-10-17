3e7af5754f4f1aa8eae33ed52ce74ef8c9671601
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2021-10-17 12:21:46 +0600

    add  attendance status

--- models/AttendanceModel.js
+++ models/AttendanceModel.js
@@ -4,7 +4,7 @@ const AttendanceModel = {
 
   setAttendanceStart: async (id, startTime) => {
     try {
-      const sqlStart = 'INSERT INTO `attendance`(`user_id`,`start_time`) VALUES (?,?)';
+      const sqlStart = 'INSERT INTO `attendance`(`user_id`,`start`) VALUES (?,?)';
       const values = [id, startTime]
       const [rows] = await dbConnect.promise().execute(sqlStart, values);
       return rows;
@@ -16,8 +16,8 @@ const AttendanceModel = {
 
   setAttendanceEnd: async (id, endTime) => {
     try {
-      const sqlEnd = 'INSERT INTO `attendance`(`user_id`,`end_time`) VALUES (?,?)';
-      const values = [id, endTime]
+      const sqlEnd = 'UPDATE  attendance SET  end = ?, status = 0 WHERE user_id = ?  AND  status = 1';
+      const values = [endTime, id]
       const [rows] = await dbConnect.promise().execute(sqlEnd, values);
       return rows;
     } catch (err) {
@@ -27,7 +27,7 @@ const AttendanceModel = {
   },
 }
 
-// sql query for  attendance 
+// sql query for  attendance
 
 // SELECT	TIMEDIFF(SEC_TO_TIME(SUM(TIME_TO_SEC(TIME(end_time)))),  SEC_TO_TIME(SUM(TIME_TO_SEC(TIME(start_time))))) AS total_time  FROM attendance WHERE user_id = 9 AND DATE(create_at) = '2021-10-16'
 module.exports = AttendanceModel;
