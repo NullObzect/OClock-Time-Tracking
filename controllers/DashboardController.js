1fcbdbbc2f2d959186b9a69d848c13e90ac1123a
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2021-10-18 22:01:07 +0600

    user work history add

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -1,6 +1,26 @@
+const AttendanceModel = require('../models/AttendanceModel')
+const UserModel = require('../models/UserModel')
+
 const DashboardController = {
-  getDashboard: (req, res) => {
-    res.render('pages/dashboard')
+  getDashboard: async (req, res) => {
+    console.log(req.user)
+    const [user] = await UserModel.findUserByEmail(req.user.userMailFormDB)
+    const [startData] = await AttendanceModel.getStartData(user.id)
+    const history = await AttendanceModel.getHistory(user.id)
+    const today = await AttendanceModel.getToday(user.id)
+    const grandTotal = await AttendanceModel.getTodayTotal(user.id)
+    console.log(grandTotal)
+    console.log(history)
+    if (startData) {
+      const { start } = startData
+      res.render('pages/dashboard', {
+        start, history, today, grandTotal,
+      })
+    } else {
+      res.render('pages/dashboard', {
+        start: 0, history, today, grandTotal,
+      })
+    }
   },
 }
 module.exports = DashboardController
