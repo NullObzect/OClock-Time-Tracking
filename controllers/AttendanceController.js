fb3275d05381b48eee7964f700f0b2441cab4423
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2021-10-22 22:43:15 +0600

    Update attendanceController.js
    Api to attendance start and end  data response

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,43 +1,38 @@
 const AttendanceModel = require('../models/AttendanceModel');
 
 const AttendanceController = {
-
+  // Current start time value store in database
   attendanceStart: async (req, res) => {
     try {
       const { id } = req.user
-      const { startTime } = req.body;
-      console.log(startTime)
-
-      const insertedAttendanceStart = await AttendanceModel.setAttendanceStart(id, startTime)
+      const insertedAttendanceStart = await AttendanceModel.setAttendanceStart(id)
+      const [result] = await AttendanceModel.getStartData(id)
+      const { start } = result
       if (insertedAttendanceStart.errno) {
-        res.send('Error')
-      } else {
-        res.redirect('/dashboard')
+        return res.send('Error')
       }
+      return res.json(start)
     } catch (err) {
       console.log('====>Error form AttendanceController/userAttendance', err);
       return err;
     }
   },
-
+  // Current end time value store in database
   attendanceEnd: async (req, res) => {
     try {
       const { id } = req.user
-      const { endTime } = req.body;
-      console.log(endTime)
-
-      const insertedAttendanceEnd = await AttendanceModel.setAttendanceEnd(id, endTime)
+      const insertedAttendanceEnd = await AttendanceModel.setAttendanceEnd(id)
+      const getTodayData = await AttendanceModel.getToday(id)
+      const [todayTotalData] = await AttendanceModel.todayTotal(id)
       if (insertedAttendanceEnd.errno) {
-        res.send('Error')
-      } else {
-        res.redirect('/dashboard')
+        return res.send('Error')
       }
+      return res.json({ getTodayData, todayTotalData })
     } catch (err) {
       console.log('====>Error form AttendanceController/userAttendance', err);
       return err;
     }
   },
-
 }
 
 module.exports = AttendanceController
