666ff48dfb29cd490ddb85cd96a1196730b1fa76
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2021-10-23 18:54:24 +0600

    update DasgboardController.js
    destructuring variable

--- controllers/DashboardController.js
+++ controllers/DashboardController.js
@@ -2,15 +2,17 @@ const AttendanceModel = require('../models/AttendanceModel')
 const UserModel = require('../models/UserModel')
 
 const DashboardController = {
+  // Get today , this week history & today, this week total value from database
   getDashboard: async (req, res) => {
-    console.log(req.user)
     const [user] = await UserModel.findUserByEmail(req.user.userMailFormDB)
     const [startData] = await AttendanceModel.getStartData(user.id)
     const weekHistory = await AttendanceModel.getWeekHistory(user.id)
     const today = await AttendanceModel.getToday(user.id)
-    const todayTotal = await AttendanceModel.todayTotal(user.id)
-    const weekTotal = await AttendanceModel.weekTotal(user.id)
-    console.log(todayTotal)
+    const [tTotal] = await AttendanceModel.todayTotal(user.id)
+    const [wTotal] = await AttendanceModel.weekTotal(user.id)
+    const { todayTotal } = tTotal
+    const { weekTotal } = wTotal
+
     if (startData) {
       const { start } = startData
       res.render('pages/dashboard', {
