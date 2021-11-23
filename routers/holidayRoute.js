ea0e81d3e73b77f3012869853b313bd80a201c96
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2021-11-24 00:15:34 +0600

    holiday and leave
    for holiday and leave day api request in database

--- routers/holidayRoute.js
+++ routers/holidayRoute.js
@@ -5,4 +5,10 @@ const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse'
 
 router.get('/add-holiday', decorateHtmlResponse('Add Holiday'), HolidayController.getAddHolidayPage)
 router.post('/add-holiday', decorateHtmlResponse('Add Holiday'), HolidayController.addHoliday)
+router.get('/holiday-list', decorateHtmlResponse('Holidays'), HolidayController.holidayList)
+router.get('/edit/holiday/:id', decorateHtmlResponse('Edit Holiday'), HolidayController.getEditHolidayPage)
+router.post('/edit/holiday', decorateHtmlResponse('Edit Holiday'), HolidayController.getUpdateHoliday)
+
+router.get('/delete/holiday/:id', HolidayController.getDeleteHoliday)
+
 module.exports = router
--- routers/leavedayRoute.js
+++ routers/leavedayRoute.js
@@ -4,5 +4,9 @@ const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse'
 
 router.get('/add-leaveday', decorateHtmlResponse('Add Leaveday'), LeaveController.getAddLeavedayPage)
 router.post('/add-leaveday', decorateHtmlResponse('Add Leaveday'), LeaveController.addLeaveday)
+router.get('/leavedays-list', decorateHtmlResponse('Leavedays'), LeaveController.employeeLeavedaysList)
+
+router.get('/edit/leaveday/:id', decorateHtmlResponse('Edit Leavedays'), LeaveController.getEditLeavePage)
+router.post('/edit/leaveday', LeaveController.setLeaveData)
 
 module.exports = router
