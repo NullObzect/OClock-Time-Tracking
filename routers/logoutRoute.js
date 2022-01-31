573a9b1dfd874067461b4a38b3d04ac21b12ca24
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-01-31 21:29:20 +0600

    Update logoutRoute.js

--- routers/logoutRoute.js
+++ routers/logoutRoute.js
@@ -4,4 +4,6 @@ const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse'
 
 router.get('/logout', decorateHtmlResponse('Logout'), LogoutController.logout)
 
+router.get('/active-user', decorateHtmlResponse('Logout'), LogoutController.logout)
+
 module.exports = router
