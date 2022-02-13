ab8e5d85cad1c70044f68b35e0cac18837cce095
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-02-13 19:17:39 +0600

    Holidays this month and this year

--- models/HolidayModel.js
+++ models/HolidayModel.js
@@ -31,6 +31,24 @@ const HolidayModel = {
     const [row] = await dbConnect.promise().execute(deleteSql, value)
     return row.affectedRows;
   },
+  thisMonthOffdaysInHolidays: async () => {
+    const query = 'SELECT DAYNAME(H.start) AS stratDayName, DATEDIFF(end,start) + 1 countDays FROM holidays AS h  WHERE  DATE(H.start) BETWEEN date( CURRENT_DATE - INTERVAL  DAYOFMONTH(CURRENT_DATE)-1 DAY) and date(CURRENT_DATE)'
+    const [rows] = await dbConnect.promise().execute(query);
+    return rows;
+  },
+  // this year holidays
+  thisYearHolidays: async () => {
+    const query = "SELECT DATE_FORMAT(start,'%Y-%m-%d') AS holidayStartDate, DATEDIFF(end,start) + 1 countHolidays FROM holidays AS h  WHERE DATE(H.start) BETWEEN date( CURRENT_DATE - INTERVAL  DAYOFYEAR(CURRENT_DATE)-1 DAY) and date(CURRENT_DATE)"
+    const [rows] = await dbConnect.promise().execute(query);
+    return rows;
+  },
+  // this year holidays in year
+
+  thisYearOffdaysInHolidays: async () => {
+    const query = 'SELECT DAYNAME(H.start) AS stratDayName, DATEDIFF(end,start) + 1 countDays FROM holidays AS h  WHERE  DATE(H.start) BETWEEN date( CURRENT_DATE - INTERVAL  DAYOFYEAR(CURRENT_DATE)-1 DAY) and date(CURRENT_DATE)'
+    const [rows] = await dbConnect.promise().execute(query);
+    return rows;
+  },
 
 }
 
