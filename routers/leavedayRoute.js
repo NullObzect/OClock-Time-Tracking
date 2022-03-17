994595504df159e90f31e6337384cac361f34cc8
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-03-17 18:09:19 +0600

    leave API req

--- routers/leavedayRoute.js
+++ routers/leavedayRoute.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 const LeaveController = require('../controllers/LeaveController')
+const { checkLogin } = require('../middleware/common/AuthMiddleware')
+
 const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse')
 
-router.get('/add-leaveday', decorateHtmlResponse('Add Leaveday'), LeaveController.getAddLeavedayPage)
+// router.get('/leavedays', decorateHtmlResponse('Leavedays'), checkLogin, LeaveController.getAddLeavedayPage)
+// router.get('/add-leaveday', decorateHtmlResponse('Add Leaveday'), LeaveController.getAddLeavedayPage)
 router.post('/add-leaveday', decorateHtmlResponse('Add Leaveday'), LeaveController.addLeaveday)
-router.get('/leavedays-list', decorateHtmlResponse('Leavedays'), LeaveController.employeeLeavedaysList)
+router.get('/leavedays', decorateHtmlResponse('Leavedays'), checkLogin, LeaveController.employeeLeavedaysList)
 
 router.get('/edit/leaveday/:id', decorateHtmlResponse('Edit Leavedays'), LeaveController.getEditLeavePage)
 router.post('/edit/leaveday', LeaveController.setLeaveData)
