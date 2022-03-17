0c9489cd944a0983df9abf280588cb2d9a03b457
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-03-17 17:59:50 +0600

    dynamic option values

---
+++ public/js/optionValues.js
@@ -0,0 +1,62 @@
+// const helperJs = require('../public/js/halper')
+import { aJAXPostRequest } from './halper.js';
+
+const optionValueChangeBtn = document.querySelectorAll('.opt-val-change-btn');
+const optionValueInput = document.querySelectorAll('.option-value');
+const optionIdInput = document.querySelectorAll('.optionId');
+const optionValueSaveBtn = document.querySelectorAll('.opt-val-save-btn');
+
+for (let i = 0; i < optionValueChangeBtn.length; i += 1) {
+  optionValueChangeBtn[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    optionValueSaveBtn[i].style.display = 'block';
+    optionValueChangeBtn[i].style.display = 'none';
+    // optionValueInput[i].classList.add("bottom-border", "option-value-focus");
+    optionValueInput[i].focus();
+  });
+}
+// when user click on save button
+for (let i = 0; i < optionValueSaveBtn.length; i += 1) {
+  optionValueSaveBtn[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    const optionValue = optionValueInput[i].value;
+    const optionId = optionIdInput[i].value;
+    if (optionValue === '') {
+      alert('Please enter option value');
+      return;
+    }
+    const data = {
+      optionValue,
+      optionId,
+    };
+    console.log({ data });
+    aJAXPostRequest('/options/option-values/update-option-value', data);
+
+    optionValueSaveBtn[i].style.display = 'none';
+    optionValueChangeBtn[i].style.display = 'block';
+    optionValueInput[i].textContent = data.optionValue;
+  });
+}
+
+/*
+      fetch("/options/option-values/update-option-value", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          optionValue: optionValue,
+          optionId: optionId,
+        }),
+      })
+        .then((res) => console.log(res))
+
+        .then((data) => {
+          console.log({ data });
+          if (data.success) {
+            alert("Option Value Updated");
+          }
+        })
+        .catch((err) => console.log(err)); */
+
+// return window.location.replace("/options/option-values");
