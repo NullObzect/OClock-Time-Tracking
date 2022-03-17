6892158098384a9cde132dda734194498879983b
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-03-17 18:01:40 +0600

    for reports

--- routers/attendanceRoute.js
+++ routers/attendanceRoute.js
@@ -6,6 +6,7 @@ const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse'
 
 router.post('/attendance-start', AttendanceController.attendanceStart)
 router.post('/attendance-end', AttendanceController.attendanceEnd)
+router.post('/update-end-time', AttendanceController.updateEndTime)
 
 // router.get('/start-time', AttendanceController.startTime)
 
@@ -19,6 +20,8 @@ router.get('/reports/between/two-date', ReportController.reportBetweenTwoDate)
 // router.get('/reports/between/two-date/:id', ReportController.reportBetweenTwoDate)
 
 // router.get('/reports/employee/:id', decorateHtmlResponse('Employee Report'), ReportController.reportEmployees)
-//router.get('/reports/between/two-date/:id', ReportController.reportBetweenTwoDateForAdmin)
+// router.get('/reports/between/two-date/:id', ReportController.reportBetweenTwoDateForAdmin)
 
+// for log
+// SELECT user_id, in_time, out_time, TIME(MIN(start)) AS start, TIME(MAX(end)) AS end, TIME_FORMAT( O.option_value, '%h') AS work_hour, TIMEDIFF(SEC_TO_TIME(SUM(TIME_TO_SEC(end))), SEC_TO_TIME(SUM(TIME_TO_SEC(start)))) as totalWorkTime, work_details  FROM attendance JOIN options AS O ON o.option_title = 'fixed time'   WHERE user_id = 18 AND DATE(create_at) = CURRENT_DATE
 module.exports = router;
