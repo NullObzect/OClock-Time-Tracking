b1a34297975bbd9430f5be23ff6f98fe35e20832
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-03-17 18:09:02 +0600

    options API request

--- routers/optionsRoute.js
+++ routers/optionsRoute.js
@@ -5,10 +5,14 @@ const decorateHtmlResponse = require('../middleware/common/decorateHtmlResponse'
 const { checkLogin } = require('../middleware/common/AuthMiddleware')
 
 router.get('/', decorateHtmlResponse('Options'), checkLogin, OptionsController.getOptionsList)
+router.get('/option-values', decorateHtmlResponse('Options'), checkLogin, OptionsController.getOptionValues)
 router.get('/projects', decorateHtmlResponse('Projects'), checkLogin, OptionsController.getProjects)
 router.post('/projects', decorateHtmlResponse('Projects'), checkLogin, OptionsController.createProject)
 router.get('/notice', decorateHtmlResponse('Notice'), checkLogin, OptionsController.getNotice)
 router.post('/notice', decorateHtmlResponse('Notice'), checkLogin, OptionsController.sendNotice)
 router.post('/contact-admin', decorateHtmlResponse('Notice'), checkLogin, OptionsController.contactAdmin)
+// update option values
+router.post('/option-values/update-option-value', OptionsController.getUpdateOptionValues)
+router.post('/projects/update-project', OptionsController.getUpdateProject)
 
 module.exports = router
