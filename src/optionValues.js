b932ab70ff11c670f59abf9d401e01cece991847
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-04-04 23:44:27 +0600

    add new optionValue.js

---
+++ src/optionValues.js
@@ -0,0 +1,62 @@
+import Toastify from './toastify.js';
+
+const inputWarning = Toastify({
+  text: 'Please enter option value',
+  className: 'info',
+})
+const successToast = Toastify({
+  text: 'Change Successfully',
+  className: 'info',
+})
+
+const optionValueChangeBtn = document.querySelectorAll('.opt-val-change-btn');
+const optionValueInput = document.querySelectorAll('.option-value');
+const optionIdInput = document.querySelectorAll('.optionId');
+const optionValueSaveBtn = document.querySelectorAll('.opt-val-save-btn');
+
+for (let i = 0; i < optionValueChangeBtn.length; i += 1) {
+  optionValueChangeBtn[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    optionValueSaveBtn[i].style.display = 'block';
+    optionValueChangeBtn[i].style.display = 'none';
+    // optionValueInput[i].classList.add("bottom-border", "option-value-focus");
+    optionValueInput[i].focus();
+  });
+}
+// when user click on save button
+for (let i = 0; i < optionValueSaveBtn.length; i += 1) {
+  optionValueSaveBtn[i].addEventListener('click', (e) => {
+    e.preventDefault();
+    const optionValue = optionValueInput[i].value;
+
+    const optionId = optionIdInput[i].value;
+    if (optionValue === '') {
+      inputWarning.showToast();
+      return;
+    }
+    const data = {
+      optionValue,
+      optionId,
+    };
+    aJAXPostRequest('/options/option-values/update-option-value', data);
+
+    optionValueSaveBtn[i].style.display = 'none';
+    optionValueChangeBtn[i].style.display = 'block';
+    optionValueInput[i].textContent = data.optionValue;
+  });
+}
+
+//  AJAX post request function
+const aJAXPostRequest = (url, values) => new Promise((resolve, reject) => {
+  fetch(url, {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json; charset=UTF-8' },
+
+    body: JSON.stringify(values),
+  }).then((res) => {
+    if (res.status === 200) {
+      successToast.showToast()
+    }
+  })
+    .catch((err) => console.log(err));
+})
