1688bf4bf73f5387fddd415283cf9f8214a1dc5e
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-05-23 13:56:39 +0600

    add leave type

--- routers/leavedayRoute.js
+++ routers/leavedayRoute.js
@@ -13,4 +13,10 @@ router.post('/leavedays/update', LeaveController.setLeaveData)
 router.get('/leavedays/delete/:id', LeaveController.getDeleteLeaveday)
 router.get('/leavedays/between-two-date', LeaveController.getLeavedayListBetweenTwoDate)
 
+// Request leave
+
+router.get('/request-leave', decorateHtmlResponse('Request Leave'), checkLogin, LeaveController.requestLeaveList)
+
+router.post('/add-leave-type', LeaveController.addLeaveType)
+
 module.exports = router
