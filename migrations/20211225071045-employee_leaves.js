869077cf0b6323e5b5fa9b642c85644dbc0d1d6d
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-05-26 23:58:00 +0600

    migration for employee leave table

--- migrations/20211225071045-employee_leaves.js
+++ migrations/20211225071045-employee_leaves.js
@@ -19,9 +19,13 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      reason: {
+      type_id: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.INTEGER,
+      },
+      create_at: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()'),
       },
 
     });
