d1fa5f3ef62395be0bb82c7bcafaf745d4b35518
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-06-01 00:10:20 +0600

    validation for manual input

--- utilities/formater.js
+++ utilities/formater.js
@@ -117,6 +117,23 @@ const Formater = {
     return `${hours}:${minutes}`;
   },
 
+  time24HrTo12Hr: (time) => {
+    // Check correct time format and split into components
+    time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+
+    if (time.length > 1) { // If time format correct
+      time = time.slice(1); // Remove full string match value
+      time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+      time[0] = +time[0] % 12 || 12; // Adjust hours
+    }
+    // return adjusted time or original string
+    const getTime = time.join('')
+    if (getTime.length === 7) {
+      return `0${getTime}`
+    }
+    return `${getTime}`;
+  },
+
 }
 
 module.exports = Formater
