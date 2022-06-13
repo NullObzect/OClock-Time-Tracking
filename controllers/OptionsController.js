4d1448bb3ed370997afa897d9d41103ec9ba7c2e
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-06-13 09:39:12 +0600

    remove notice

--- controllers/OptionsController.js
+++ controllers/OptionsController.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-await-in-loop */
 
 const OptionsModel = require('../models/OptionsModel')
@@ -50,35 +51,6 @@ const OptionsController = {
     } catch (error) {
       console.log('', error);
     }
-  },
-
-  getNotice: async (req, res) => {
-    const { user } = req
-    if (user.user_role === 'admin') {
-      const users = await userModel.getAllUsersList()
-      const notices = await OptionsModel.getAllNotice()
-      res.render('pages/notice', { users, notices, user })
-    } else {
-      const notices = await OptionsModel.userNotice(user.id)
-      res.render('pages/notice', { notices, user })
-    }
-  },
-  sendNotice: async (req, res) => {
-    const { userId, noticeDetails } = req.body
-    try {
-      const result = await OptionsModel.sendNotice(userId, noticeDetails)
-      console.log(result)
-      res.redirect('/options/notice')
-    } catch (error) {
-      console.log(error)
-    }
-  },
-  contactAdmin: async (req, res) => {
-    const { userId, noticeDetails } = req.body
-    const { user } = req
-    const senderId = user.id
-    const result = await OptionsModel.contactAdmin(userId, senderId, noticeDetails)
-    res.redirect('/options/notice')
   },
   getUpdateOptionValues: async (req, res) => {
     try {
