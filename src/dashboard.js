cb7d7125d49fec269e104a956edeec5b7e3ea279
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-07-18 12:58:18 +0600

    update: move internal js to external js in dashboard js file

--- src/dashboard.js
+++ src/dashboard.js
@@ -102,3 +102,99 @@ for (let i = 0; i < optCancelBtn.length; i++) {
     saveBtnForEndTime[i].style.display = 'none';
   });
 }
+
+// add internal script in dashboard
+
+const userNameBtn = document.querySelectorAll('.userBtn')
+const userDetailsModal = document.querySelector('#userDetails')
+const userDetailsModalClose = document.querySelector('.user-modal-close')
+const container = userDetailsModal.querySelector('.user-user-container')
+console.log(userNameBtn)
+for (let i = 0; i < userNameBtn.length; i++) {
+  userNameBtn[i].addEventListener('click', async (e) => {
+    const element = e.target
+    const userId = element.dataset.userid
+    const api = await fetch(`/dashboard/user-today-details-for-admin/${userId}`)
+    const data = await api.json()
+    const {
+      userInfo, todayReportDetails, weekReportDetails, todayWorkList, lateCountThisWeek,
+    } = data
+
+    const bodyData = `  <div class="user-details-head">
+          <div class="user-details">
+            <div class="img">
+             ${userInfo.avatar !== null && userInfo.avatar.match(/^(http|https):/g)
+    ? `<img src=\"${userInfo.avatar}\"/>`
+    : `<img src=\"/uploads/avatars/${userInfo.avatar !== null ? userInfo.avatar : userInfo.gender == 'male' ? 'demo-avatar.png' : 'female-demo-avatar.png'}\" />`}
+            </div>
+            <div class="user-info">
+            <div class="user-name">${userInfo.user_name}</div>
+            <div class="user-mail">${userInfo.user_mail}</div>
+            </div>
+          </div>
+          <div class="report-btn"><a href="/reports/${userId}">Report</a></div>
+        </div>
+        <div class="user-today-info">
+          <table>
+            <tr>
+              <td>Entry :</td>
+              <td>${todayReportDetails.start} <span class="${todayReportDetails.inTimeExtraOrLess.length === 6 ? 'low' : todayReportDetails.inTimeExtraOrLess === '' ? ' ' : 'high'}" >${todayReportDetails.inTimeExtraOrLess.length === 5 ? `+${todayReportDetails.inTimeExtraOrLess}` : todayReportDetails.inTimeExtraOrLess}</span></td>
+            </tr>
+            <tr>
+              <td>Exit :</td>
+              <td>${todayReportDetails.end} <span class="${todayReportDetails.outTimeExtraOrLess.length === 5 ? 'low' : todayReportDetails.outTimeExtraOrLess === '' ? ' ' : 'high'}" >${todayReportDetails.outTimeExtraOrLess.length === 5 ? `-${todayReportDetails.outTimeExtraOrLess}` : todayReportDetails.outTimeExtraOrLess === '' ? ' ' : `+${todayReportDetails.outTimeExtraOrLess.slice(1)}`}</span></td>
+            </tr>
+            <tr>
+              <td>Today Total Work : </td>
+              <td>${todayReportDetails.todayTotal} hr <span class="${todayReportDetails.isLowOrHighClassForToday}">${todayReportDetails.isTotalExtraOrLessHr}</span></td>
+            </tr>
+            <tr>
+              <td>Weekly Workday :</td>
+              <td>${weekReportDetails.totalWorkingDays}/${weekReportDetails.fixedWorkdays} days <span class="${weekReportDetails.classLowOrHighForDay}">${weekReportDetails.daysIsLowOrHigh}</span></td>
+            </tr>
+            <tr>
+              <td>Weekly Work :</td>
+              <td>${weekReportDetails.workingTotalHr}/${weekReportDetails.fixedTotalHr} hr  <span class="${weekReportDetails.classLowOrHighForHr || ''}">${weekReportDetails.isTotalExtraOrLessHr}</span></td>
+            </tr>
+            <tr>
+              <td>Late Count in Week :</td>
+              <td>${lateCountThisWeek} days</td>
+            </tr>
+
+          </table>
+        </div>
+        <div class="user-work-details">
+          <div class="working-user">
+            <div class="card-header">
+              <h2>Today Details</h2>
+            </div>
+
+            <table>
+              <thead>
+                <tr>
+                  <td>Project</td>
+                  <td>Details</td>
+                  <td>Start</td>
+                  <td>End</td>
+                  <td>total</td>
+                </tr>
+              </thead>
+              <tbody>
+                ${todayWorkList.map((tw) => `<tr>
+                  <td>${tw.project_name}</td>
+                  <td>${tw.work_details}</td>
+                  <td>${tw.start}</td>
+                  <td>${tw.end}</td>
+                  <td>${tw.total}</td>
+                </tr>`)}
+              </tbody>
+            </table>
+          </div>
+        </div>`
+    container.innerHTML = bodyData
+    userDetailsModal.style.display = 'block'
+  })
+}
+userDetailsModalClose.addEventListener('click', () => {
+  userDetailsModal.style.display = 'none'
+})
