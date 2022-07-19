6979c05ab66d36242358789f239c6dc3a84984e9
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-07-19 22:03:30 +0600

    delete: index.js file

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const config = {
   entry: {
-    index: './src/index.js',
+    
     // test: './src/test.js',
     report: ['@babel/polyfill', './src/js/report.js'],
     timeTracking: ['@babel/polyfill', './src/js/timeTracking.js'],
