34ff3ef538c982c2265d34b0bb3ec9fa929bb2ad
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-07-19 11:58:04 +0600

    add chart.js

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,26 +1,62 @@
-const ctx = document.getElementById('myChart').getContext('2d');
-const myChart = new Chart(ctx, {
+const bar = document.getElementById('myBar').getContext('2d');
+const pie = document.getElementById('myPie').getContext('2d');
+const doughnut = document.getElementById('myDoughnut').getContext('2d');
+const myChart = new Chart(bar, {
   type: 'bar',
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(16, 48, 71, 1,1)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)',
-      ],
-    }],
+    labels: ['Sat', 'Sun', 'Mon', 'Tue', 'Wed', 'Thu'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [42, 46, 43, 36, 0, 0],
+        backgroundColor: [
+          'rgba(16, 48, 71, 1)',
+          'rgba(123, 129, 161, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
   },
   options: {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
-    },
+    responsive: true,
   },
 });
+const pieConfig = {
+  type: 'polarArea',
+  data: {
+    labels: ['Total', 'Running', 'Need'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [120, 100, 20],
+        backgroundColor: [
+          'rgba(16, 48, 71, 1)',
+          'rgba(123, 129, 161, 1)',
+          'rgba(255,114,114)',
+        ],
+      },
+    ],
+  },
+  options: {},
+};
+const pieChart = new Chart(pie, pieConfig)
+
+const doughnutConfig = {
+  type: 'doughnut',
+  data: {
+    labels: ['User', 'Working', 'Leave'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [40, 30, 10],
+        backgroundColor: [
+          'rgba(16, 48, 71, 1)',
+          'rgba(123, 129, 161, 1)',
+          'rgba(255,114,114)',
+        ],
+      },
+    ],
+  },
+  options: {},
+};
+const doughnutChart = new Chart(doughnut, doughnutConfig)
