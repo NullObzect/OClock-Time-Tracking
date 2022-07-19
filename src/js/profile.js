e9bcf85344d420ad1f6e71f3250ddf62a1d25bc0
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2022-07-19 21:40:26 +0600

    remove: console log

--- src/js/profile.js
+++ src/js/profile.js
@@ -59,12 +59,9 @@ form.addEventListener('click', () => {
 // Select to profile image change
 
 function readURL(input) {
-  console.log(input)
   if (input.files && input.files[0]) {
     const reader = new FileReader();
-    console.log(reader);
     reader.onload = function (e) {
-      console.log(e);
       document.querySelector('#img').setAttribute('src', e.target.result);
     };
     reader.readAsDataURL(input.files[0]);
